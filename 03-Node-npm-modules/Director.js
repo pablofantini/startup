3d22d411e11c9f3f706599bbae55ee499bf546d3
Pablo Fantini <pablo.fantini@globant.com>
2015-01-14 09:53:04 -0300

    start tp3

--- 03-Node-npm-modules/Director.js
+++ 03-Node-npm-modules/Director.js
@@ -5,29 +5,34 @@ module.exports = function (name) {
   var attributes = {};
   attributes.name = name;
   /* Public functions */
-  
-  
+
+
   /**
    * Set attribute
    * @param {String} attr
    * @param {Object} value
    */
-  this.set = function(attr, value) {
+  this.set = function (attr, value) {
     attributes[attr] = value;
   }
 
   /**
    * Get attribute
    * @param {String} attr
+   * @return {Object} attribute value
    */
-  this.get = function(attr) {
+  this.get = function (attr) {
     return attributes[attr];
   }
-  
+
   /**
    * Director speak
-   */ 
-  this.speak = function() {
-    console.log(attributes.name + ' say: ' + this.get('quotes').join(' and ')); 
+   * @return {String} director quotes
+   */
+  this.speak = function () {
+    var quotes = this.get('quotes') === 'object' ? this.get('quotes').join(' and ') : this.get('quotes');
+    var say = attributes.name + ' say: ' + quotes;
+    console.log(say);
+    return say;
   }
 }
--- 03-Node-npm-modules/main.js
+++ 03-Node-npm-modules/main.js
@@ -1,9 +1,19 @@
-var Movie = require("./Movie.js");
-var Director = require("./Director.js");
+// jQuery module
+var $ = require('jquery');
+
+// Application modules
+var Movie = require("./Movie.js"),
+    Director = require("./Director.js");
 
 
+// Application start
 var alien = new Movie();
 var ridleyScott = new Director('Ridley Scott');
 ridleyScott.set('quotes', ['Cast is everything.', 'Do what ...']);
 alien.set('director', ridleyScott);
-alien.get('director').speak();
+var directorSpeak = alien.get('director').speak();
+
+// show director speak in html
+$( document ).ready(function() {
+    $('#speak').html(directorSpeak);
+});
